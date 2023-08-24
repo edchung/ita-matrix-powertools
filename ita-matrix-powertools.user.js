ef45d088b8b2f5c144a6a65d270e21283107f130
Edwin <ed@edchung.com>
2023-08-23 17:41:41 -0700

    change ver -ec

--- ita-matrix-powertools.user.js
+++ ita-matrix-powertools.user.js
@@ -2,7 +2,7 @@
 // @name ITA Matrix Powertools
 // @namespace https://github.com/adamhwang/ita-matrix-powertools
 // @description Adds new features and builds fare purchase links for ITA Matrix
-// @version 0.55.12
+// @version 0.55.12-ec
 // @icon https://raw.githubusercontent.com/adamhwang/ita-matrix-powertools/master/icons/icon32.png
 // @require https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
 // @grant GM.getValue
